0bc94d8ee80ad68d5c06c10ba5736aab2b04846d
Adrian Gherle <adriangherle@gmail.com>
2014-07-01 08:34:29 -0700

    Added info to readme file, Modified content and added 'id's to some tags in the index.html file, Added jQuery objects and removed some unnecesary code for BlackJack.js file

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 BlackJack-attempt_v2
 ====================
+
+This is a simplified version of the original BlackJack card game. The game has only
+one player and a dealer.
+The player starts the game first by getting two cards from the deck. The player can
+get more cards (Hit), or stand (Stand button). Dealer needs to get a minimum of 17
+points unless the player is bust in which case dealer only takes two cards and wins.
+To win, player needs to total more than the dealer without going over 21 (unles 
+22 points are made from the first two cards). At the same total the dealer wins.
+
+Card values:
+1 (Ace) = 11 points or 1 point if total is over 21 from more than 2 cards
+2  - 10 = value represented by the number
+11 - 13 = 10 points (J, Q, K)
--- js/BlackJack.js
+++ js/BlackJack.js
@@ -1,312 +1,254 @@
-	function Deck() {
-		var cardsUsed, queue;
-		cardsUsed = 0;
-		this.cards = [];
-		queue = new Queue();
+function Deck() {
+	var cardsUsed, queue;
+	cardsUsed = 0;
+	this.cards = [];
 
-		this.count = function() {
-			return this.cards.length;
-		}
+	this.count = function() {
+		return this.cards.length;
+	}
 
-		this.init = function() {
-			// start loop of suits, s
-			for(s = 1; s <= 4; s++) {
-				// start loop of ranks, r
-				for(r = 1; r <= 13; r++) {
-					this.cards.push(new Card(r, s));
-				}
+	this.init = function() {
+		// start loop of suits, s
+		for(s = 1; s <= 4; s++) {
+			// start loop of ranks, r
+			for(r = 1; r <= 13; r++) {
+				this.cards.push(new Card(r, s));
 			}
-		}
-
-		this.shuffle = function() {
-			for (var i = this.cards.length - 1; i >= 0; i--) {
-				var random = Math.floor(Math.random() * (i + 1));
-				var temp = this.cards[i];
-				this.cards[i] = this.cards[random];
-				this.cards[random] = temp;
-				// display the cards after beeing shuffled
-				// this.cards[i].show();
-			}
-		}
-
-		this.dealCard = function() {
-			if(cardsUsed == this.cards.length) {
-				alert("There are no more cards left in the deck." +
-					"\nPlease start a new game.");
-			}
-			cardsUsed++;
-			queue.enqueue(this.cards[cardsUsed - 1]);
-			return this.cards[cardsUsed - 1];
-		}
-
-		this.showQueue = function() {
-			return queue.showQueue();
 		}
 	}
 
-
-	function Card(rank, suit) {
-		var suits;
-		this.rank = rank;
-		this.suit = suit;
-		suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
-		this.show = function() {
-			console.log(this.rank + " of " + this.suit);
-			return (rank + " of " + suits[suit - 1]);
+	this.shuffle = function() {
+		for (var i = this.cards.length - 1; i >= 0; i--) {
+			var random = Math.floor(Math.random() * (i + 1));
+			var temp = this.cards[i];
+			this.cards[i] = this.cards[random];
+			this.cards[random] = temp;
 		}
 	}
 
-
-	// A queue to store all the cards used in a game
-	function Queue() {
-		var front, rear, size;
-
-		front = null;
-		rear = null;
-		size = 0;
-
-		// adds a new element at the end (rear) of the Queue
-		this.enqueue = function(card) {
-			newLink = new Link(card, null);
-			if(size == 0) {
-				front = rear = newLink;
-			} else {
-				rear.next = newLink;
-				rear = rear.next;
-			}
-			size++;
+	this.dealCard = function() {
+		if(cardsUsed == this.cards.length) {
+			alert("There are no more cards left in the deck." +
+				"\nPlease start a new game.");
 		}
+		cardsUsed++;
+		return this.cards[cardsUsed - 1];
+	}
+}
 
-		// returns and removes the element that is at the top of the Queue
-		this.dequeue = function(card) {
-			var temp;
-			if(size == 0) {
-				alert("There are no more elements in the Queue.");
-			} else {
-				temp = front.item;
-				front = front.next;
-				size--;
-			}
-			return temp;
-		}
 
-		// returns a copy of the element that is at the front of the Queue without 
-		// removing the element
-		this.peek = function() {
-			if(size == 0) {
-				return "There are no more elements in the Queue !"
-			}else {
-				return front.item;
-			}
-		}
+function Card(rank, suit) {
+	var suits, ranks;
+	this.rank = rank;
+	this.suit = suit;
+	ranks = ["Ace", '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+	suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+	this.show = function() {
+		console.log(this.rank + " of " + this.suit);
+		return (ranks[rank - 1] + " of " + suits[suit - 1]);
+	}
+}
 
-		// returns a string containing all the cards in the queue
-		this.showQueue = function() {
-			var temp, message;
-			temp = front;
-			message = "\n";
-			for(var i = size; i > 0; i--){
-				message += temp.item.show() + "\n";
-				if(i != 1) {
-					temp = temp.next;
-				}
-			}
-			return message;
-		}
 
-		// returns the number of elements in the queue
-		this.getSize = function() {
-			return size;
-		}
+function Player(deck) {
+	var flag, hand, card;
+	flag = true;
+	this.deck = deck;
+	hand = new Hand();
+
+	this.go = function() {
+		card = deck.dealCard();
+		hand.addCard(card);
+		// display the cards dealt
+		$("#player").append("<li>Card:  " + card.show() + "</li>");
+		var $total = "<p>Player total is: " + this.getTotal() + "</p>";
+		$("#p-total").html($total); // display total for player
+		$("#p-total").css("background-color","lightGreen");
+		$("#p-total").width("250px");
 	}
 
-	// constructs a new link
-	function Link(item, next) {
-		this.item = item;
-		this.next = next;
+	this.getTotal = function() {
+		return hand.getTotal();
 	}
 
-
-	function Player(deck) {
-		var flag, hand, card;
-		flag = true;
-		this.deck = deck;
-		hand = new Hand();
-
-		hand.addCard(deck.dealCard());
-
-		this.go = function() {
-			hand.addCard(deck.dealCard());
-		}
-
-		this.getTotal = function() {
-			return hand.getTotal();
-		}
-
-		this.getHand = function() {
-			return hand;
-		}
-
-		this.getCardsInHand = function() {
-			return hand.getCardsInHand();
-		}
-
-		this.showHand = function() {
-			return hand.showHand();
-		}
+	this.getHand = function() {
+		return hand;
 	}
 
-	function Dealer(deck) {
-		var hand;
-		this.deck = deck;
-		hand = new Hand();
-		hand.addCard(deck.dealCard());
-
-		this.go = function(){
-			do{
-				hand.addCard(deck.dealCard());
-			} while(hand.getTotal() < 17);
-		}
-
-		this.getTotal = function() {
-			return hand.getTotal();
-		}
-
-		this.getHand = function() {
-			return hand;
-		}
-
-		this.getCardsInHand = function() {
-			return hand.getCardsInHand();
-		}
-
-		this.showHand = function() {
-			return hand.showHand();
-		}
+	this.getCardsInHand = function() {
+		return hand.getCardsInHand();
 	}
 
-
-	function Hand() {
-		var total, hasAce;
-		this.hand = [];
-		total = 0;
-		hasAce = false;
-		
-		this.getHand = function() {
-			return this.hand;
-		}
-
-		this.getTotal = function() {
-			return total;
-		}
-
-		this.getCardsInHand = function() {
-			return this.hand.length;
-		}
-
-		this.showHand = function() {
-			var message = "";
-			for(var i = 0; i < this.hand.length; i++) {
-				message += "\nCard number " + (i + 1) + " :  " + this.hand[i].show();
-			}
-			message += "\nTotal points:   " + this.getTotal() + "\n";
-			return message;
-		}
-
-		this.addCard = function(card) {
-			var isAce, val;
-			isAce = false;
-			val = card.rank;
-
-			this.hand.push(card);
-			if(val == 1 || val == 11) {
-				hasAce = true;
-				isAce = true;	
-			}
-			if(val > 11) {
-				val = 10;
-			}
-			if(isAce) {
-				if(hand.length != 2 && total + 11 > 21) {
-					total += 1;
-					hasAce = false;
-				} else {
-					total += 11;
-				}
-			} else if(hasAce) {
-				if(total + val > 21) {
-					total += val - 10;
-					hasAce = false;
-				} else {
-					total += val;
-				}
-			} else {
-				total += val;
-			}
-		}
+	this.showHand = function() {
+		return hand.showHand();
 	}
+}
+
+function Dealer(deck) {
+	var hand, card;
+	this.deck = deck;
+	hand = new Hand();
 	
+	this.go = function() {
+		card = deck.dealCard();
+		hand.addCard(card);
+		// display the cards dealt
+		$("#dealer").append("<li>Card:  " + card.show() + "</li>")
+		var $total = "<p>Dealer total is: " + this.getTotal() + "</p>";
+		$("#d-total").html($total);  // display total for dealer
+		$("#d-total").css("background-color","lightGreen");
+		$("#d-total").width("250px");
+	}
 
-	function BlackJack() {
-		var player, dealer, deck;
+	this.getTotal = function() {
+		return hand.getTotal();
+	}
 
-		deck = new Deck();
-		deck.init();
-		deck.shuffle();
+	this.getHand = function() {
+		return hand;
+	}
 
-		player = new Player(deck);
-		dealer = new Dealer(deck);
+	this.getCardsInHand = function() {
+		return hand.getCardsInHand();
+	}
 
-		dealAgain = function(p) {
-			var answer = prompt("\nHere are your results:\n " +
-					p.showHand() + "\nYour total is:  " + p.getTotal() + "\n" +
-					"\nWould you like to get another card ?" +
-					"\n(y for YES, anything else for NO)");
-			if(answer == "y" || answer == "Y") {
-				return true;
-			} else {
-				return false;
-			}
+	this.showHand = function() {
+		return hand.showHand();
+	}
+}
+
+
+function Hand() {
+	var total, hasAce;
+	this.hand = [];
+	total = 0;
+	hasAce = false;
+	
+	this.getHand = function() {
+		return this.hand;
+	}
+
+	this.getTotal = function() {
+		return total;
+	}
+
+	this.getCardsInHand = function() {
+		return this.hand.length;
+	}
+
+	this.showHand = function() {
+		var message = "";
+		for(var i = 0; i < this.hand.length; i++) {
+			message += "\nCard number " + (i + 1) + " :  " + this.hand[i].show();
 		}
+		message += "\nTotal points:   " + this.getTotal() + "\n";
+		return message;
+	}
 
-		this.play = function() {
-			var flag;
-			flag = true;
-			do {
-				player.go();
-				if(player.getCardsInHand() == 2 && player.getTotal() > 20) {
-					alert("You have BlackJack.\n" + player.showHand() + 		
-						"\nMoving on to next player.");
-					flag = false;
-				} else {
-					if(player.getTotal() > 21) {
-						flag = false;
-					} else {
-						flag = dealAgain(player);
-					}
-				}
-			} while (flag);
+	this.addCard = function(card) {
+		var isAce, val, hasAce;
+		isAce = false;
+		val = card.rank;
+		this.hand.push(card);
 
-			if(player.getTotal() > 21) {
-				alert("You loose! \nHere are the results: \n" + player.showHand() +
-					"\nGame Over !");
+		if(val == 1) {
+			val = 11;
+			hasAce = true;
+			isAce = true;	
+		}
+		if(val > 10) {
+			val = 10;
+		}
+		// update the total points value (ace (1) can count as 1 or 11 points)
+		if(this.hand.length == 2 && total + val > 21) {
+			total = 21;
+		} else if(isAce) {
+			if(total + 11 > 21) {
+				total += 1;
+				hasAce = false;
 			} else {
-				dealer.go();
-				if(dealer.getTotal() > 21 || player.getTotal() > dealer.getTotal()) {
-					alert("You win !\nHere are the results: \n"
-						+ "\nPLAYER \n" + player.showHand()
-						+ "\nDEALER \n" + dealer.showHand());
-				} else {
-					// player.getTotal() <= dealer.getTotal()) {
-					alert("You loose! \nHere are the results: \n" + 
-						"\nPLAYER \n" + player.showHand() +
-						"\n " + "\nDEALER \n" + dealer.showHand());
-				}
+				total += 11;
 			}
-			alert("Here are all the cards that were used from the deck " +
-			"in the order they were used: \n" + deck.showQueue());	
+		} else if(hasAce && total + val > 21) {
+			total += val - 10;
+			hasAce = false;
+		} else {
+			total += val;
+		}
+	}
+}
+
+
+function BlackJack() {
+	var player, dealer, deck, bust;
+	bust = false;
+	deck = new Deck();
+	deck.init();
+	deck.shuffle();
+
+	player = new Player(deck);
+	dealer = new Dealer(deck);
+
+	this.hit = function() {
+		player.go();
+		checkTotal();
+	}
+
+	this.stand = function() {
+		finishGame();
+	}
+
+	var checkTotal = function() {
+		if(player.getTotal() == 21) {
+			finishGame();
+		} else if(player.getTotal() > 21) {
+			bust = true;
+			finishGame();
 		}
 	}
 
-	b = new BlackJack();
-	b.play();
+	this.play = function() {
+		$("button#hit").on("click", b.hit);
+		$("button#stand").on("click", b.stand);
+		player.go();
+		dealer.go();
+		player.go();
+		checkTotal();
+	}
+
+	var finishGame = function() {
+		$("button#hit").fadeOut('slow');
+		$("button#stand").fadeOut('slow');
+		var $result;
+		dealer.go();
+		if(bust) {  // player bust
+			$result = "YOU LOST!";
+		} else {
+			while(dealer.getTotal() < 17 || dealer.getTotal() < player.getTotal()) {
+				dealer.go();
+			}
+			if(dealer.getTotal() > 21) { // dealer bust
+				$result = "YOU WIN!";
+			} else if(dealer.getTotal() >= player.getTotal()) {
+				$result = "YOU LOST!";
+			}
+		}
+		// Display the winner
+		$('#display').html($result);
+	}
+}
+
+
+$(document).ready(function() {
+	$('button#start').on('click', function() { // starts a new game
+		b = new BlackJack();
+		b.play();
+		
+		$("button#game").fadeIn('slow');
+		$(this).fadeOut('slow');
+	});
+	
+	$("button#game").on("click", function() { // starts a new game
+		location.reload();
+	});
+});
